4d3ce2ac41e6306828f3d4b160c8c317d349f728
Neo40 <tron36.cc@gmail.com>
2023-11-09 00:05:09 -0300

    Adicao de getters e setters

--- src/dominio/Bootcamp.java
+++ src/dominio/Bootcamp.java
@@ -5,4 +5,20 @@ import java.util.List;
 public class Bootcamp {
     private String nome;
     private List<Dev> devs;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Dev> getDevs() {
+        return devs;
+    }
+
+    public void setDevs(List<Dev> devs) {
+        this.devs = devs;
+    }
 }
--- src/dominio/Dev.java
+++ src/dominio/Dev.java
@@ -2,4 +2,12 @@ package dominio;
 
 public class Dev {
     private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
