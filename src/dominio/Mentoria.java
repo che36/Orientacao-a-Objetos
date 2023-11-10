6c9d035097388e3f75ba0b06810d4cabe6aaa9b0
Neo40 <tron36.cc@gmail.com>
2023-11-09 22:05:10 -0300

    Abstracao da classe Conteudo

--- src/dominio/Mentoria.java
+++ src/dominio/Mentoria.java
@@ -2,26 +2,9 @@ package dominio;
 
 import java.time.LocalDateTime;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
     private LocalDateTime data;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDateTime getData() {
         return data;
