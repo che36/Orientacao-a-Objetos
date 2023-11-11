339b34d86ebf040fd19c690a5c85708660a05619
Neo40 <tron36.cc@gmail.com>
2023-11-11 17:45:11 -0300

    Adicao de Objeto curso na classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import dominio.Curso;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Java Básico");
+        cursoJava.setDescricao("Aprenda Conceitos Basicos de Java.");
+        cursoJava.setCargaHoraria(8);
 
     }
 }
