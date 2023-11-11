7534e4c47be2ae57b5615ba7b07cbcebded344d5
Neo40 <tron36.cc@gmail.com>
2023-11-10 21:38:56 -0300

    Atributo lista adicionado

--- src/dominio/Dev.java
+++ src/dominio/Dev.java
@@ -1,7 +1,28 @@
 package dominio;
 
+import java.util.List;
+
 public class Dev {
     private String nome;
+    private List<Conteudo> conteudosInscritos;
+    private List<Conteudo> conteudosConcluidos;
+
+    public List<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(List<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public List<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(List<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
 
     public String getNome() {
         return nome;
